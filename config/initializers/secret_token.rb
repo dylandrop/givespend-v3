9be41740174ef944b4f3cdcbf7727de1c1d45353
Dylan <dylandrop@gmail.com>
2013-10-31 17:13:42 -0400

    Initialized project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GivespendV3::Application.config.secret_token = '2f789aa42ca39980b61509597a9563cd736cde926025bb2553581cec8458b3fb1067ba88a7ba627d971d13657ce96c1a1ced9f1ecb008ad82aea10ca9bb5ad75'
