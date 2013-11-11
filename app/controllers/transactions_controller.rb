6997a09558c1bf98a7db999c0e87162e58fe896a
Dylan <dylandrop@gmail.com>
2013-11-11 18:24:56 -0500

    Transactions now saved

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,4 +2,14 @@ class TransactionsController < ApplicationController
   def new
     @transaction = Item.find(params[:item_id]).transactions.build
   end
+
+  def create
+    @transaction = Item.find(params[:transaction][:item_id]).transactions.build
+    @transaction.stripe_card_token = params[:stripe_card_token]
+    if @transaction.save_with_card
+      redirect_to root_path, notice: "Thank you for your purchase!"
+    else
+      render :new
+    end
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,17 @@
 class Transaction < ActiveRecord::Base
   belongs_to :item
-  attr_accessible :status, :stripe_customer_token
+  attr_accessor :stripe_card_token
+  attr_accessible :status, :stripe_customer_token, :stripe_card_token
+
+  def save_with_card
+    if valid?
+      customer = Stripe::Customer.create(card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  rescue Stripe::InvalidRequestError => e
+    logger.error "Stripe error while creating customer: #{e.message}"
+    errors.add :base, "There was a problem with your credit card."
+    false
+  end
 end
