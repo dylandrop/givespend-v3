39de1e8b3e5011e2166d9b898c1e2466d22de1cb
Dylan <dylandrop@gmail.com>
2013-11-11 14:59:55 -0500

    added price in dollars function

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,10 @@ class Item < ActiveRecord::Base
   has_one :image, as: :imageable
   before_save :price_to_cents
   accepts_nested_attributes_for :image
+  
+  def price_in_dollars
+    '%.2f' % (price.to_i/100.0)
+  end
 
   private
 
