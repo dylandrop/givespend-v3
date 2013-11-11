fc33442c7cd7c7b48717c3702447fd7938a63adf
Dylan <dylandrop@gmail.com>
2013-11-11 18:50:01 -0500

    Made job for processing payments

---
+++ app/jobs/payout_to_seller_job.rb
@@ -0,0 +1,16 @@
+class PayoutToSellerJob
+  def self.distribute_payment txn_id
+    transaction = Transaction.includes(item: [:user]).find(txn_id)
+    item = transaction.item
+    seller = item.user
+    Stripe::Charge.create({
+      amount: (item.price * (1 - item.nonprofit_percentage / 100.0)).to_i,
+      currency: 'usd',
+      customer: transaction.stripe_customer_token,
+      description: "Txn: #{txn_id} Item: #{item.id}",
+      application_fee: (item.price * item.nonprofit_percentage / 100.0).to_i},
+      seller.secret_key
+    )
+    transaction.update_attribute(:status, "purchased")
+  end
+end
